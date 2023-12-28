eb97d0b6ae1bc75fc81a0950b5022edfbbb44917
neeru0713 <neerurani1307@gmail.com>
2023-12-28 23:29:09 +0530

    grid view for products with whishlist feature completed

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,16 +1,24 @@
-
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 
 interface ProductCardProps {
   name: string;
   price: string;
   imageUrl: string;
-  rating: number
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ name, price, imageUrl, rating }) => {
+const ProductCard: React.FC<ProductCardProps> = ({ name, price, imageUrl }) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLikeClick = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="product-card">
+      <div className="heart-icon" onClick={handleLikeClick}>
+        {isLiked ? <AiFillHeart color="red" /> : <AiOutlineHeart />}
+      </div>
       <img src={imageUrl} alt={name} />
       <h3>{name}</h3>
       <p>{price}</p>
--- src/components/ProductsPage.tsx
+++ src/components/ProductsPage.tsx
@@ -4,6 +4,12 @@ import logo from "../assets/images/zevi-logo.webp";
 import SearchBar from "./SearchBar";
 import Filter from "./Filter";
 import ProductCard from './ProductCard';
+import stylishJacket from "../assets/images/stylishJacket.jpg"
+import designerDress from "../assets/images/designer-dress.webp"
+import patternDress from "../assets/images/patterndress.jpg"
+import formalSuit from "../assets/images/formalSuit.jpeg"
+import leatherShirt from "../assets/images/leatherShirt.jpeg"
+
 const ProductsPage: React.FC = () => {
   const brands: string[] = ["Mango", "H&M"];
   const prices: string[] = ["Under 500", "1000 To 3000"];
@@ -11,25 +17,31 @@ const ProductsPage: React.FC = () => {
 
 
   const products = [
-    { id: 1, name: 'Product 1', price: '$50', imageUrl: 'url_to_image_1' },
-    { id: 2, name: 'Product 2', price: '$100', imageUrl: 'url_to_image_2' },
-    { id: 3, name: 'Product 3', price: '$75', imageUrl: 'url_to_image_3' },
-    { id: 4, name: 'Product 4', price: '$120', imageUrl: 'url_to_image_4' },
-    // Add more products as needed
+    { id: 1, name: 'Product 1', price: '$50', imageUrl: stylishJacket, rating: 3 },
+    { id: 2, name: 'Product 2', price: '$100', imageUrl: designerDress, rating: 3 },
+    { id: 3, name: 'Product 3', price: '$75', imageUrl: patternDress, rating: 3 },
+    { id: 4, name: 'Product 4', price: '$120', imageUrl: formalSuit, rating: 3 },
+    { id: 4, name: 'Product 4', price: '$120', imageUrl: leatherShirt, rating: 3 },
+    { id: 4, name: 'Product 4', price: '$120', imageUrl: designerDress, rating: 3 },
+    { id: 4, name: 'Product 4', price: '$120', imageUrl: formalSuit, rating: 3 },
+    { id: 4, name: 'Product 4', price: '$120', imageUrl: patternDress, rating: 3 },
+   
   ];
 
   return (
     <div className="products-page">
-      <img src={logo} id="landing-page-icon" />
+     
 
       <header>
         <SearchBar className="products-page-search" />
       </header>
+
       <h1>Search Results</h1>
+      
       <section>
         <div id="filter-section">
           <Filter heading="BRAND" data={brands} />
-          <hr />
+          <hr/>
           <Filter heading="PRICE RANGE" data={prices} />
         </div>
 
@@ -40,7 +52,7 @@ const ProductsPage: React.FC = () => {
               name={product.name}
               price={product.price}
               imageUrl={product.imageUrl}
-              // rating={product.rating}
+              
             />
           ))}
         </div>
