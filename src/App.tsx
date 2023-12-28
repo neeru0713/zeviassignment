d2b339df4a1ef8c2092b7036df2f7b1ae3dfa592
neeru0713 <neerurani1307@gmail.com>
2023-12-28 15:38:13 +0530

    intial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "./styles/App.scss";
+// import TrendingCloths from "./components/TrendingCloths";
+// import SearchBar from "./components/SearchBar";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Trending Cloths</h1>
+      {/* <SearchBar />
+      <TrendingCloths /> */}
     </div>
   );
-}
+};
 
 export default App;
